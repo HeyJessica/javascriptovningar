14c3dfae7c702e7e4be8eec7dfa8d4f8feee5ffa
HeyJessica <jess.gabrielsson@gmail.com>
2018-09-22 16:55:12 +0200

    added loppis text

---
+++ addElements/script.js
@@ -0,0 +1,10 @@
+
+
+
+for(var i=0; i<=4; i++){
+    var para = document.createElement("p");
+    var node = document.createTextNode("Wow! En Loppis!");
+    para.appendChild(node);
+    var element = document.getElementById("loppisar");
+    element.appendChild(para);
+}

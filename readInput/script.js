a535a74dd7e6f6e77926362b40b192d93e39eca2
HeyJessica <jess.gabrielsson@gmail.com>
2018-09-22 16:30:42 +0200

    lagt till javascrpit

---
+++ readInput/script.js
@@ -0,0 +1,12 @@
+function setInputName() {
+
+    var inputNamn = document.getElementById("inputNamn").value;
+   document.getElementById("nameTitle").innerHTML="Namn: "+inputNamn;
+}
+
+function setInputEpost(){
+    var inputEpost = document.getElementById("inputEpost").value;
+    document.getElementById("epostTitle").innerHTML="Epost: "+inputEpost;
+}
+
+
